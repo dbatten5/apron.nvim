b4dcc0efc8784cc82e255762a937be8e4ed9fc65
Dom Batten <dominic.batten@googlemail.com>
2026-04-14 14:21:27 +0100

    Add github utility to get pr template

--- lua/apron/github.lua
+++ lua/apron/github.lua
@@ -1,5 +1,37 @@
 local M = {}
 
+---Get the content of a PR template by template name
+---@param template_name? string The template name. If not provided and there are
+---available templates then return the first one.
+---@return string|nil the PR template if found or nil otherwise
+function M.get_pr_template(template_name)
+	local cmd = "gh repo view --json pullRequestTemplates"
+	local output = vim.fn.system(cmd)
+
+	if vim.v.shell_error ~= 0 or output == "" then
+		return nil
+	end
+
+	local ok, data = pcall(vim.json.decode, output)
+	if not ok or not data.pullRequestTemplates then
+		return nil
+	end
+
+	if not template_name and #data.pullRequestTemplates > 0 then
+		return data.pullRequestTemplates[1].body
+	end
+
+	local selected_template = nil
+	for _, item in ipairs(data.pullRequestTemplates) do
+		if item.filename == template_name then
+			selected_template = item.body
+			break
+		end
+	end
+
+	return selected_template
+end
+
 ---Create a pull request on GitHub
 ---@param title string PR title
 ---@param description string PR description
